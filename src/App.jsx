185193a1be3656b423af076f9d9b5073b17fdc80
AlexTcw <etaverac429@alumno.uaemex.mx>
2024-05-08 15:37:47 -0600

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Sidebar from "./components/shared/Sidebar.jsx";
+import {RiMenu3Fill} from "react-icons/ri";
+
+
 export default function App() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
+        <h1 className="bg-[#262837] w-full min-h-screen">
+            <Sidebar/>
+            {/*Menu movil*/}
+            <nav className={"bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0"}>
+                <button><RiMenu3Fill/></button>
+            </nav>
         </h1>
     )
 }
